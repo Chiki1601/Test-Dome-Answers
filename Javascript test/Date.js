e957258d8e32cd3e141c3c6932a9450469b7bc2b
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-01-09 11:18:59 +0530

    Delete Javascript test directory

--- Javascript test/Date.js
+++
@@ -1,9 +0,0 @@
-function formatDate(userDate) {
-  userDate = new Date(userDate);
-  y = userDate.getFullYear().toString();
-  m = (userDate.getMonth() + 1).toString();
-  d = userDate.getDate().toString();
-  if (m.length == 1) m = '0' + m;
-  if (d.length == 1) d = '0' + d;
-  return y + m + d;
-}
--- Javascript test/remove property.js
+++
@@ -1,3 +0,0 @@
-function removeProperty(obj, prop) {
-  return (Object.keys(obj).indexOf(prop) > -1) ? delete obj[prop] : false;
-}
