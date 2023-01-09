24737bcfc4f3b3d5a0743f89d275e7eba5e0c6f8
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-01-09 10:21:16 +0530

    Create ice cream machine.py

---
+++ Python test/ice cream machine.py
@@ -0,0 +1,16 @@
+from itertools import product
+
+
+class IceCreamMachine:
+
+    def __init__(self, ingredients, toppings):
+        self.ingredients = ingredients
+        self.toppings = toppings
+
+    def scoops(self):
+        return [list(p) for p in product(self.ingredients, self.toppings)]
+
+
+if __name__ == "__main__":
+    machine = IceCreamMachine(["vanilla", "chocolate"], ["chocolate sauce"])
+    print(machine.scoops()) #should print[['vanilla', 'chocolate sauce'], ['chocolate', 'chocolate sauce']]
